f323bd72e6d293597aed7bc1c725a0bdd402263f
Andreas Sommer <sommer@belsignum.com>
2023-09-26 22:02:16 +0200

    [TASK] Wrap in function caller

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,32 @@
 <?php
 
-if (TYPO3_MODE == "BE")
-{
-    /** @var \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer */
-    $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Page\PageRenderer::class
-    );
-    $pageRenderer->loadRequireJsModule('TYPO3/CMS/PowermailVoucher/Module/List');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
-$container = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Extbase\Object\Container\Container::class
-);
-$container
-    ->registerImplementation(
-        \In2code\Powermail\Domain\Model\Field::class,
-        \Belsignum\PowermailVoucher\Domain\Model\Field::class
-    );
+call_user_func(
+    function ()
+    {
+        if (TYPO3_MODE == "BE")
+        {
+            /** @var \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer */
+            $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+                \TYPO3\CMS\Core\Page\PageRenderer::class
+            );
+            $pageRenderer->loadRequireJsModule('TYPO3/CMS/PowermailVoucher/Module/List');
+        }
 
-$container->registerImplementation(
-    \In2code\Powermail\Domain\Model\Mail::class,
-    \Belsignum\PowermailVoucher\Domain\Model\Mail::class
+        $container = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Extbase\Object\Container\Container::class
+        );
+        $container
+            ->registerImplementation(
+                \In2code\Powermail\Domain\Model\Field::class,
+                \Belsignum\PowermailVoucher\Domain\Model\Field::class
+            );
+
+        $container->registerImplementation(
+            \In2code\Powermail\Domain\Model\Mail::class,
+            \Belsignum\PowermailVoucher\Domain\Model\Mail::class
+        );
+    }
 );
+
